0ea18566fb8d152aeb7db8c55b3140802ce2bb15
Nicolas Moraes <nicolas.moraeslsilva@gmail.com>
2024-07-06 15:07:12 -0300

    :art: setup shadcn/ui and TailwindCSS

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,8 @@
 import './globals.css';
 
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
 
-const inter = Inter({ subsets: ['latin'] });
+import { satoshi } from '@/lib/fonts';
 
 export const metadata: Metadata = {
   title: 'Next Movies',
@@ -17,7 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='pt-BR'>
-      <body className={inter.className}>{children}</body>
+      <body className={`${satoshi.variable}`}>{children}</body>
     </html>
   );
 }
---
+++ src/lib/fonts.ts
@@ -0,0 +1,27 @@
+import localFont from 'next/font/local';
+
+export const satoshi = localFont({
+  variable: '--font-satoshi',
+  src: [
+    {
+      path: '../../public/fonts/Satoshi-Light.woff2',
+      weight: '300',
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/Satoshi-Regular.woff2',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/Satoshi-Medium.woff2',
+      weight: '500',
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/Satoshi-Bold.woff2',
+      weight: '700',
+      style: 'normal',
+    },
+  ],
+});
