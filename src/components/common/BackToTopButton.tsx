aa1a7975b64a42f4129cd3289b850f0fb405cd66
Nicolas Moraes <nicolas.moraeslsilva@gmail.com>
2024-07-20 16:56:46 -0300

    :art: update BackToTopButton component styles

--- src/components/common/BackToTopButton.tsx
+++ src/components/common/BackToTopButton.tsx
@@ -24,15 +24,15 @@ export default function BackToTopButton() {
     <AnimatePresence>
       {showButton && (
         <motion.button
-          animate={{ opacity: 1 }}
-          exit={{ opacity: 0 }}
-          initial={{ opacity: 0 }}
-          className='fixed bottom-10 right-10 z-50 rounded-full bg-primary p-2 text-primary-foreground shadow-[0_0_10px_-5px_#000000]'
+          animate={{ y: 0 }}
+          exit={{ y: 100 }}
+          initial={{ y: 100, x: '-50%' }}
+          className='fixed inset-x-1/2 bottom-10 z-50 inline-flex w-max items-center justify-center rounded-full bg-primary px-4 py-2 text-sm font-bold text-primary-foreground shadow-[0_0_10px_-5px_#000000] transition-colors duration-150 hover:bg-primary/90'
           transition={{ duration: 0.2 }}
           onClick={() => window.scrollTo({ top: 0 })}
         >
-          <CaretUp className='h-10 w-10' />
-          <span className='sr-only'>Voltar para o topo</span>
+          <CaretUp className='mr-2 h-4 w-4' weight='bold' />
+          <span>Voltar para o topo</span>
         </motion.button>
       )}
     </AnimatePresence>
